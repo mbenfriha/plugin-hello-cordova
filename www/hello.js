1fae57f1e1ee6d184be457c6b92dc54bc19210de
mohamed.benfriha@epitech.eu <mohamed.benfriha@epitech.eu@epitech.eu>
2019-04-16 16:26:33 +0200

    update js

--- www/hello.js
+++ www/hello.js
@@ -1,7 +1,23 @@
 /*global cordova, module*/
 
-module.exports = {
-    say: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "say", [name]);
-    }
+var exec = cordova.require('cordova/exec');
+
+var Hello = function() {
+    console.log('HELLO READY');
 };
+
+Hello.prototype.say = function(msg, onSuccess, onError) {
+    var errorCallback = function(obj) {
+        onError(obj);
+    };
+
+    var successCallback = function(obj) {
+        onSuccess(obj);
+    };
+
+    exec(successCallback, errorCallback, 'Hello', 'say', [msg]);
+};
+
+if (typeof module != 'undefined' && module.exports) {
+    module.exports = Hello;
+}
